85785bda4e9b8fc9109c52cfb09fd29135c827c9
TanaFedos <krupik@tut.by>
2021-11-27 15:53:16 +0100

    Add files via upload

--- exercise.e2e.js
+++ exercise.e2e.js
@@ -1,4 +1,4 @@
-import { username, password } from './fixtures.js'
+import { username, password, userFullName } from './fixtures.js'
 
 describe('Authentication page', () => {
 
@@ -26,7 +26,7 @@ describe('Authentication page', () => {
         loginButton.click();
 
         const userNameDropdown = $('[data-toggle="dropdown"]');
-        expect(userNameDropdown.getText()).toEqual('Lenka');
+        expect(userNameDropdown.getText()).toEqual(userFullName);
     });
 
     it('should logout', () => {
@@ -54,4 +54,53 @@ describe('Authentication page', () => {
     });
 });
 
+describe('My account page', () => {
 
+    beforeEach(() => {
+        browser.reloadSession();
+        browser.url('/login?back=my-account');
+    });
+
+    it('my account ', () => {
+
+        const emailField = $('#email');
+        expect(emailField).toBeDisplayed();
+        expect(emailField).toBeEnabled();
+
+        const passwordField = $('#passwd');
+        expect(passwordField).toBeDisplayed();
+        expect(passwordField).toBeEnabled();
+
+        const loginButton = $('#SubmitLogin');
+        expect(loginButton.getText()).toEqual('Sign in');
+
+        emailField.setValue(username);
+        passwordField.setValue(password);
+        loginButton.click();
+
+        const userNameDropdown = $('[data-toggle="dropdown"]');
+        expect(userNameDropdown.getText()).toEqual(userFullName);
+
+        const fieldOrders = $('a[title="Orders"]').$('span');
+        expect(fieldOrders).toBeDisplayed();
+        expect(fieldOrders).toBeEnabled();
+
+        const fieldCredit = $('a[title="Credit slips"]').$('span');
+        expect(fieldCredit).toBeDisplayed();
+        expect(fieldCredit).toBeEnabled();
+
+        const fieldAddresses = $('a[title="Addresses"]').$('span');
+        expect(fieldAddresses).toBeDisplayed();
+        expect(fieldAddresses).toBeEnabled();
+
+        const fieldPersonal = $('a[title="Information"]').$('span');
+        expect(fieldPersonal).toBeDisplayed();
+        expect(fieldPersonal).toBeEnabled();
+
+        const fieldHome = $('a[title="Home"]').$('span');
+        expect(fieldHome).toBeDisplayed();
+        fieldHome.click();
+        browser.saveScreenshot('main_page.png');
+
+    });
+});
